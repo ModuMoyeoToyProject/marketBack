6a428d8cb8c71a7f7529c2c01c124b8291e720e8
라즈베리 <gtj9966@gmail.com>
2022-12-04 22:01:44 +0900

    Delete userExample

--- userExample/userExample/user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.user_register, name='userRegister'),
-    path('userInfo/', views.display_user, name='userInfo')
-]
