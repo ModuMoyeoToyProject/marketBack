ab03a2f697c689715347f16d28c50e097de0bfed
라즈베리 <gtj9966@gmail.com>
2022-12-05 19:33:27 +0900

    create player urls

--- BackServer/player/urls.py
+++ BackServer/player/urls.py
@@ -3,5 +3,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('<str:playerID>/action/fishing', views.player_update)
+    path('player/action/hunting', views.player_hunting),
+    path('player/action/harvesting', views.player_harvesting),
+    path('player/action/taming', views.player_taming),
+    path('player/action/farming', views.player_farming),
+    path('player/action/fishing', views.player_fishing)
 ]
--- BackServer/player/views.py
+++ BackServer/player/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
-def player_update(request, userSeq):
+def player_hunting(request, userSeq):
+    return render(request, 'player/player_update.html')
+
+def player_harvesting(request, userSeq):
+    return render(request, 'player/player_update.html')
+
+def player_taming(request, userSeq):
+    return render(request, 'player/player_update.html')
+
+def player_farming(request, userSeq):
+    return render(request, 'player/player_update.html')
+
+def player_fishing(request, userSeq):
     return render(request, 'player/player_update.html')
