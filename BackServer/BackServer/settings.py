db630fa2c73d222fa7c9ab1ee713c617fff2d7e7
라즈베리 <gtj9966@gmail.com>
2022-12-28 19:43:15 +0900

    setting.py의 INSTALLED_APPS의 corsheaders 제거

--- BackServer/BackServer/settings.py
+++ BackServer/BackServer/settings.py
@@ -37,9 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'corsheaders',
-
     'account',
     'player',
     'system',
