af5d3157016a614041970fafe963d9c38c7e0e8e
Hyunseok Lim <hyunseoki@outlook.kr>
2022-12-30 17:00:47 +0900

    CustomUser 구현

--- BackServer/account/admin.py
+++ BackServer/account/admin.py
@@ -1,3 +1,60 @@
+from django.contrib.auth.models import Group as Django_Group
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from .models import *
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+class UserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {
+            "fields": (
+                "nickname",
+                "email",
+                "gender",
+                "picture",
+                "status_message",
+        ),},),
+        (_("Permissions"), {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+        ),},),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {"fields": ("username", "password1", "password2")}),
+        (_("Personal info"), {
+            "fields": (
+                "nickname",
+                "email",
+                "gender",
+                "picture",
+                "status_message",
+        ),},),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    list_display = ("username", "nickname", "email", "is_staff")
+    search_fields = ("username", "nickname", "email")
+    ordering = ("is_staff", "username",)
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Django_Group)
+admin.site.register(Group)
+admin.site.register(Account)
--- BackServer/account/models.py
+++ BackServer/account/models.py
@@ -1,4 +1,25 @@
+from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+class Group(Group):
+    class Meta:
+        verbose_name = "그룹"
+        verbose_name_plural = "그룹"
+
+class User(AbstractUser):
+    nickname = models.CharField(verbose_name='닉네임', max_length=16, null=True, blank=False)
+    email = models.EmailField(_("email address"), blank=False)
+    gender = models.BooleanField(verbose_name='성별', null=True, blank=True)
+    picture = models.ImageField(verbose_name='프로필 사진', upload_to='www/images/profile', null=True, blank=True)
+    status_message = models.CharField(verbose_name='상태 메시지', max_length=32, null=True, blank=True)
+    first_name = None
+    last_name = None
+
+    class Meta:
+        verbose_name = "사용자"
+        verbose_name_plural = "사용자"
+    
 
 class Account(models.Model):
     accountID = models.AutoField(primary_key=True)
@@ -8,4 +29,3 @@ class Account(models.Model):
     gender = models.CharField(max_length=255)
     username = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
-
