9d11dcf3880604d714521160b9548b0e4d7d2401
IngGu <mushroom041215@gmail.com>
2023-01-04 21:11:50 +0900

    player앱 selling-buying뷰 추가

--- BackServer/player/views.py
+++ BackServer/player/views.py
@@ -135,8 +135,69 @@ def player_fishing(request):
 
     return render(request, 'player/player_update.html')
 
+@csrf_exempt
 def player_buying(request):
-    pass
-
+    if request.method == 'POST':
+        response = dict()
+        Buying_itemID = request.POST['ItemID']
+        PlayerCharacterID = request.POST['PlayerCharacterID']
+        player_character = PlayerCharacter.objects.get(id=PlayerCharacterID)
+        player_inventory = Inventory.objects.get(PlayerCharacterID=player_character)
+        item_info = ItemInfo.objects.get(itemID=Buying_itemID)
+        response['Bmoney'] = player_inventory.money
+        
+        if player_inventory.money >= item_info.buyingValue:
+            if Item.objects.filter(inventory=player_inventory, itemID=Buying_itemID).exists():
+                item = Item.objects.get(inventory=player_inventory, itemID=Buying_itemID)
+                response['Bitem'] = item.count
+                item.count += 1
+                player_inventory.money-=item_info.buyingValue
+                item.save()
+                player_inventory.save()
+                response['Aitem'] = item.count
+            else:
+                item = Item(inventory=player_inventory,
+                            itemID=item_info.itemID,
+                            itemName=item_info.itemName,
+                            itemType=item_info.itemType,
+                            sellingValue=item_info.sellingValue,
+                            buyingValue=item_info.buyingValue,
+                            exp=item_info.exp)
+                player_inventory.money-=item_info.buyingValue
+                player_inventory.save()
+                item.save()
+                response['Nitem'] = item.count
+            response['result'] = 'Successed'
+            response['Amoney'] = player_inventory.money
+        else:
+            response['result'] = 'NotEnoughMoney'
+            response['Amoney'] = player_inventory.money
+        return JsonResponse(response)
+    
+@csrf_exempt
 def player_selling(request):
-    pass
+    if request.method == 'POST':
+        response = dict()
+        selling_itemID = request.POST['ItemID']
+        PlayerCharacterID = request.POST['PlayerCharacterID']
+        player_character = PlayerCharacter.objects.get(id=PlayerCharacterID)
+        player_inventory = Inventory.objects.get(PlayerCharacterID=player_character)
+        response['Bmoney'] = player_inventory.money
+        if Item.objects.filter(inventory=player_inventory, itemID=selling_itemID).exists():
+            if Item.objects.get(inventory=player_inventory, itemID=selling_itemID).count > 0:
+                item_info = ItemInfo.objects.get(itemID=selling_itemID)
+                item = Item.objects.get(inventory=player_inventory, itemID=selling_itemID)
+                response['Bitem'] = item.count
+                item.count -= 1
+                player_inventory.money+=item_info.sellingValue
+                item.save()
+                player_inventory.save()
+                response['Aitem'] = item.count
+                response['result'] = 'Successed'
+                response['Amoney'] = player_inventory.money
+            else:
+                response['result'] = 'DontHaveItem'
+        else:
+            response['result'] = 'ItemDoesNotExists'
+        response['Amoney'] = player_inventory.money
+        return JsonResponse(response)
